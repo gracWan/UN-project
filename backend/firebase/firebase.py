b396ad8ac903c81c3e8fbc0bc904a638fc3952c4
Wilson <wilsonfzheng@gmail.com>
2024-10-08 19:06:21 -0400

    updated firebase folders

---
+++ backend/firebase/firebase.py
@@ -0,0 +1,10 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("credentials.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+if __name__ == "__main__":
+    print("Starting")
